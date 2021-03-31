b7645835a7f873f5b685fef3b598bf4ddd299bda
Matt Jarvis <mattj23@gmail.com>
2021-03-31 16:42:00 -0400

    save test with fixture

--- BookShuffler.Tests/ProjectTests.cs
+++ BookShuffler.Tests/ProjectTests.cs
@@ -23,24 +23,23 @@ namespace BookShuffler.Tests
         [Fact]
         public void NewProject_Saves()
         {
-            var project = ProjectViewModel.New("fake_folder");
-            var builder = project.MockBuilder();
-            var s0 = builder.AddSection(project.Root, new SectionViewModel("section0"));
-            var c0 = builder.AddCard(s0, "card0", "card0");
-            var c1 = builder.AddCard(s0, "card1", "card1");
-            var c2 = builder.AddCard(project.Root, "card2", "card2");
-
             var storage = new MockFileSystem();
             var writer = new ProjectWriter(storage);
             
-            writer.Save(project);
+            writer.Save(Project);
 
-            Assert.Contains($"fake_folder/{ProjectLoader.SectionFolderName}/{s0.Id}.yaml", storage.Values.Keys);
-            Assert.Contains($"fake_folder/{ProjectLoader.CardFolderName}/{c0.Id}.md", storage.Values.Keys);
-            Assert.Contains($"fake_folder/{ProjectLoader.CardFolderName}/{c1.Id}.md", storage.Values.Keys);
-            Assert.Contains($"fake_folder/{ProjectLoader.CardFolderName}/{c2.Id}.md", storage.Values.Keys);
-            Assert.Contains($"fake_folder/{ProjectLoader.SectionFolderName}/{project.Root.Id}.yaml", storage.Values.Keys);
-            Assert.Contains($"fake_folder/project.yaml", storage.Values.Keys);
+            Assert.Contains($"fake/{ProjectLoader.SectionFolderName}/{Project.Root.Id}.yaml", storage.Values.Keys);
+            Assert.Contains($"fake/{ProjectLoader.SectionFolderName}/{S0.Id}.yaml", storage.Values.Keys);
+            Assert.Contains($"fake/{ProjectLoader.SectionFolderName}/{S1.Id}.yaml", storage.Values.Keys);
+            Assert.Contains($"fake/{ProjectLoader.SectionFolderName}/{S2.Id}.yaml", storage.Values.Keys);
+
+            Assert.Contains($"fake/{ProjectLoader.CardFolderName}/{C0.Id}.md", storage.Values.Keys);
+            Assert.Contains($"fake/{ProjectLoader.CardFolderName}/{C1.Id}.md", storage.Values.Keys);
+            Assert.Contains($"fake/{ProjectLoader.CardFolderName}/{C2.Id}.md", storage.Values.Keys);
+            Assert.Contains($"fake/{ProjectLoader.CardFolderName}/{C3.Id}.md", storage.Values.Keys);
+            Assert.Contains($"fake/{ProjectLoader.CardFolderName}/{C4.Id}.md", storage.Values.Keys);
+
+            Assert.Contains($"fake/project.yaml", storage.Values.Keys);
         }
     }
 }
